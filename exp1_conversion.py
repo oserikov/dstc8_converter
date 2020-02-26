fb9b8ad0eacdda0400c94cde02db500788f1888f
oserikov <srkvoa@gmail.com>
2020-02-26 15:29:17 +0300

    root scripts refactoring

--- exp1_conversion.py
+++ exp1_conversion.py
@@ -6,28 +6,24 @@ clear_data_dirs()
 os.makedirs(DATA_DIR, exist_ok=True)
 os.makedirs(BOT_DATA_DIR, exist_ok=True)
 
-dialogues_files = get_dstc8_dialogue_files_list()
-dialogue_files_readt = [(f_split, json.load(open(fn))) for f_split, fn in dialogues_files]
-restaurant_dialogues = [(f_split, dialogue)
-                        for f_split, dialogues_file in dialogue_files_readt
-                        for dialogue in dialogues_file
-                        if dialogue['services'] == [DOMAIN_OF_INTEREST]]
+dstc8_dialogues_filenames = get_dstc8_dialogue_files_list()
+dstc8_dialogues = [(f_split, json.load(open(fn))) for f_split, fn in dstc8_dialogues_filenames]
+relevant_dstc8_dialogues = [(f_split, dialogue)
+                            for f_split, dialogues_file in dstc8_dialogues
+                            for dialogue in dialogues_file
+                            if dialogue['services'] == [DOMAIN_OF_INTEREST]]
 
 
-dialogues_data, slot_entries = base_dstc8_2_dstc2(restaurant_dialogues)
-mock_dialogues_data, mock_slots_dialogues_data = mockify_slots_in_dialogues(dialogues_data, slot_entries)
-mock_dialogues_w_api, mock_dialogues_w_api_slot_entries = add_db_api_calls(mock_dialogues_data, mock_slots_dialogues_data)
-
-good_slotfill_dict, templates = extract_slotfill_and_templates(mock_dialogues_w_api, mock_dialogues_w_api_slot_entries)
+dstc2_dialogues, slot_spans = add_db_api_calls(*mockify_slots_in_dialogues(*base_dstc8_2_dstc2(relevant_dstc8_dialogues)))
+slotfill_data, templates = extract_slotfill_and_templates(dstc2_dialogues, slot_spans)
 initialize_slotfill_model_config_w_paths(evaluate=False)
-unique_slot_names = list(good_slotfill_dict.keys())
+unique_slot_names = list(slotfill_data.keys())
 
-database = configure_db(mock_dialogues_w_api)
+database = configure_db(dstc2_dialogues)
 db_pkeys = database.primary_keys
 
-mock_dialogues_w_api_train, mock_dialogues_w_api_test, mock_dialogues_w_api_dev = train_test_val_split(mock_dialogues_w_api)
-train_test_val_write(mock_dialogues_w_api_train, mock_dialogues_w_api_test, mock_dialogues_w_api_dev)
+dialogues_train, dialogues_test, dialogues_dev = train_test_val_split(dstc2_dialogues)
+train_test_val_write(dialogues_train, dialogues_test, dialogues_dev)
 
 gobot_config = configure_gobot_config(db_pkeys, unique_slot_names)
 train_model(gobot_config)
-
--- exp3_conversion.py
+++ exp3_conversion.py
@@ -2,48 +2,25 @@ from deeppavlov import train_model
 from utils import *
 from constants import DOMAIN_OF_INTEREST, DATA_DIR, BOT_DATA_DIR
 
-
 clear_data_dirs()
 os.makedirs(DATA_DIR, exist_ok=True)
 os.makedirs(BOT_DATA_DIR, exist_ok=True)
 
-dialogues_files = get_dstc8_dialogue_files_list()
-dialogue_files_readt = [(f_split, json.load(open(fn))) for f_split, fn in dialogues_files]
-restaurant_dialogues = [(f_split, dialogue)
-                        for f_split, dialogues_file in dialogue_files_readt
-                        for dialogue in dialogues_file
-                        if dialogue['services'] == [DOMAIN_OF_INTEREST]]
+dstc8_dialogues_filenames = get_dstc8_dialogue_files_list()
+dstc8_dialogues = [(f_split, json.load(open(fn))) for f_split, fn in dstc8_dialogues_filenames]
+relevant_dstc8_dialogues = [(f_split, dialogue)
+                            for f_split, dialogues_file in dstc8_dialogues
+                            for dialogue in dialogues_file
+                            if dialogue['services'] == [DOMAIN_OF_INTEREST]]
 
-dialogues_data, slot_entries = base_dstc8_2_dstc2(restaurant_dialogues)
-mock_dialogues_data, mock_slots_dialogues_data = mockify_slots_in_dialogues(dialogues_data, slot_entries)
-mock_dialogues_w_api, mock_dialogues_w_api_slot_entries = add_db_api_calls(mock_dialogues_data, mock_slots_dialogues_data)
-
-act2mock_turns = dict()
-act2mock_slots = dict()
-for dialogue, slot_info in zip(mock_dialogues_w_api, mock_dialogues_w_api_slot_entries):
-    for turn_idx, turn in enumerate(dialogue):
-        if 'act' in turn.keys():
-            # will end up storing the last template(slots_info) applied for act
-            act2mock_turns[turn['act']] = turn.copy()
-            act2mock_slots[turn['act']] = slot_info[turn_idx].copy()
 
 # create the dataset applying the mappings collected above
-mock_templated_dialogues_w_api_calls = []
-mock_templated_slots_w_api_calls = []
-for dialogue, slots_info in zip(mock_dialogues_w_api, mock_dialogues_w_api_slot_entries):
-    mock_templated_dialogue = []
-    mock_templated_dialogue_slots = []
-    for turn_idx, turn in enumerate(dialogue):
-        if 'act' in turn.keys():
-            mock_templated_dialogue.append(act2mock_turns[turn['act']])
-            mock_templated_dialogue_slots.append(act2mock_slots[turn['act']])
-        else:
-            mock_templated_dialogue.append(turn.copy())
-            mock_templated_dialogue_slots.append(slots_info[turn_idx])
-    mock_templated_dialogues_w_api_calls.append(mock_templated_dialogue)
-    mock_templated_slots_w_api_calls.append(mock_templated_dialogue_slots)
+dstc2_dialogues, slot_spans = reduce_action_replics_variance(
+                                  * add_db_api_calls(
+                                      * mockify_slots_in_dialogues(
+                                          * base_dstc8_2_dstc2(relevant_dstc8_dialogues))))
 
-action_aggregated_dialogues = mock_templated_dialogues_w_api_calls.copy()
+action_aggregated_dialogues = dstc2_dialogues.copy()
 for dialogue in action_aggregated_dialogues:
     for turn in dialogue:
         if "act" in turn.keys():
@@ -62,19 +39,15 @@ for dialogue in action_aggregated_dialogues:
             turn['act'] = '+'.join(res_acts)
 
 
-good_slotfill_dict, templates = extract_slotfill_and_templates(action_aggregated_dialogues, mock_templated_slots_w_api_calls)
-unique_slot_names = list(good_slotfill_dict.keys())
+slotfill_data, templates = extract_slotfill_and_templates(action_aggregated_dialogues, slot_spans)
 initialize_slotfill_model_config_w_paths(evaluate=False)
+unique_slot_names = list(slotfill_data.keys())
 
 database = configure_db(action_aggregated_dialogues)
 db_pkeys = database.primary_keys
 
-action_aggregated_dialogues_train, action_aggregated_dialogues_test, action_aggregated_dialogues_dev = \
-    train_test_val_split(action_aggregated_dialogues)
-
-train_test_val_write(action_aggregated_dialogues_train, action_aggregated_dialogues_test,
-                     action_aggregated_dialogues_dev)
+dialogues_train, dialogues_test, dialogues_dev = train_test_val_split(action_aggregated_dialogues)
+train_test_val_write(dialogues_train, dialogues_test, dialogues_dev)
 
 gobot_config = configure_gobot_config(db_pkeys, unique_slot_names)
 train_model(gobot_config)
-
